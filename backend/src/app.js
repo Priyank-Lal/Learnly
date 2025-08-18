a65471656dfd4cff57eab54330355b2a59ebcc54
Priyank lal <ratherdrag@gmail.com>
2025-08-18 17:37:19 +0530

    Added CORS fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,13 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
+const allowedOrigins = [
+  "https://learnly-drab.vercel.app/",
+  "http://localhost:5173",
+];
+
+
+
 const authRoutes = require("./routes/auth.routes");
 const goalRoutes = require("./routes/goals.routes");
 const userRoutes = require("./routes/user.routes");
@@ -10,7 +17,7 @@ const userRoutes = require("./routes/user.routes");
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: allowedOrigins,
     credentials: true,
   })
 );
