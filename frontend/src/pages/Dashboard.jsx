90722bd67d746aad205b23e03c6b9e2e7c2b1441
Priyank lal <ratherdrag@gmail.com>
2025-08-19 16:36:45 +0530

    Test -1

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -223,10 +223,10 @@ const Dashboard = () => {
 
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors duration-300">
-      {isSubmitting && (
+      {!isSubmitting && (
         <div className="fixed inset-0 bg-gray-900/50 brightness-110 dark:brightness-100 bg-opacity-40 flex items-center justify-center z-99">
           <DotLottieReact
-            src="../../public/AIloading.lottie"
+            src="../../public/AiSearching.lottie"
             autoplay
             loop
             className="w-80 h-80 "
